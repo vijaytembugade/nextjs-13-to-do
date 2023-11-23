1bcc7ddd9b5e7e9448ea9fb76b42ce78f48169dd
guest user <guestuser@int-mac-25.domain.name>
2023-11-23 23:19:41 +0530

    feat: ticker single page

--- app/ticket/TicketList.tsx
+++ app/ticket/TicketList.tsx
@@ -1,28 +1,36 @@
 import Image from "next/image";
+import Link from "next/link";
 
-export default async function TicketList(){
-    const data = await getTickets();
-    return <div className="flex flex-wrap gap-3 align-center justify-center" >
-        {data && data?.users?.map((item: unknown)=>{
-            return (
-                <div key={item?.id} className={'card my-5'}>
+export default async function TicketList() {
+  const data = await getTickets();
+  return (
+    <div className="flex flex-wrap gap-3 align-center justify-center">
+      {data &&
+        data?.users?.map((item: unknown) => {
+          return (
+            <div key={item?.id} className={"card my-5"}>
+              <Link href={'/ticket/'+ item?.id}>
                 <h3>{item?.username}</h3>
-                <div>
-                    <p>{item?.firstName} {item?.lastName}</p>
-                </div>
-                <div>
-                    <Image src={item?.image} width={50} height={50} /> 
-                </div>
-                </div>
-            )
+              </Link>
+              <div>
+                <p>
+                  {item?.firstName} {item?.lastName}
+                </p>
+              </div>
+              <div>
+                <Image alt="any" src={item?.image} width={50} height={50} />
+              </div>
+            </div>
+          );
         })}
     </div>
+  );
 }
 
-
-async function getTickets(){
-    const responce = await fetch("https://dummyjson.com/users", {next: {'revalidate': 30}});
-    const data = await responce?.json();
-    console.log(data, "dara")
-    return data;
+async function getTickets() {
+  const responce = await fetch("https://dummyjson.com/users", {
+    next: { revalidate: 30 },
+  });
+  const data = await responce?.json();
+  return data;
 }
--- app/ticket/[id]/page.tsx
+++ app/ticket/[id]/page.tsx
@@ -3,8 +3,7 @@ import Image from "next/image";
 export default async function TicketDetails({ params }) {
     const { id } = params;
 
-    const ticket = getTicket(id);
-    console.log(ticket)
+    const ticket = await getTicket(id);
     return <main>
         <h2>Ticket Details</h2>
         <h3>{ticket?.username}</h3>
@@ -15,8 +14,7 @@ export default async function TicketDetails({ params }) {
 }
 
 async function getTicket(id: number){
-    const responce = await fetch("https://dummyjson.com/users/"+id, {next: {'revalidate': 30}});
+    const responce = await fetch("https://dummyjson.com/users/" + id, {next: {'revalidate': 30}});
     const data = await responce?.json();
-    console.log(data, "dara")
     return data;
 }
